90728c6d73bfe35d7dd45e819ddab0defc337986
Marthamouna04 <marthamouna05@gmail.com>
2025-11-03 19:16:39 +0530

    Add files via upload

---
+++ DatatypeDemo.java
@@ -0,0 +1,16 @@
+public class DatatypeDemo{
+    public static void main(String[] args){
+        byte b = 10;
+        System.out.println(b);
+        short s = 20;
+        System.out.println(s);
+        int i = 30;
+        System.out.println(i);
+        long l = 40;
+        System.out.println(l);
+        double d = 3.14;
+        System.out.println(d);
+        char ch = 'A';
+        System.out.println(ch);
+    }
+}
